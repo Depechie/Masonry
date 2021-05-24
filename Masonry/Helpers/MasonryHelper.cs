a87d3e24f0e944ca560305a5303348fda17865f3
Depechie <glenn.versweyveld@gmail.com>
2021-05-25 00:07:41 +0200

    Load extra items

--- Masonry/Helpers/MasonryHelper.cs
+++ Masonry/Helpers/MasonryHelper.cs
@@ -108,8 +108,11 @@ function invokeJSFromCSharp(data) {
         fragment.appendChild( elem );
         elems.push(elem);
     }
-    grid.appendChild( fragment );  
-    msnry.appended( elems );
+
+    grid.appendChild(fragment);
+    var imageLoad = imagesLoaded(grid);
+    imageLoad.on( 'progress', function() {  msnry.layout(); });
+    msnry.appended(elems);
 }
 
 function getItemElement(content) {
@@ -149,6 +152,7 @@ let infScroll = new InfiniteScroll( grid, {
   ##ITEMS## 
 </div>
 
+<!--
 <div class='page-load-status'>
   <div class='loader-ellips infinite-scroll-request'>
     <span class='loader-ellips__dot'></span>
@@ -159,6 +163,7 @@ let infScroll = new InfiniteScroll( grid, {
   <p class='infinite-scroll-last'>End of content</p>
   <p class='infinite-scroll-error'>No more pages to load</p>
 </div>
+-->
 
             <script src='https://unpkg.com/infinite-scroll@4/dist/infinite-scroll.pkgd.js'></script>
             <script src='https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.js'></script>
